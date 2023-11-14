212250d6303bb53875a4795aa4283e291a011edd
Suet Yee <suetyeeee@gmail.com>
2023-11-14 13:17:15 +0800

    Update

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}")
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print(f"Sum of both numbers are: {x+y}")
+print(f"Differences of both numbers are: {x-y}")
+print(f"Product of both numbers are: {x*y}")
+print(f"Quotient of both numbers are: {x/y}")
+
+age = int(input("What is your current age? "))
+print(f"Next year, you will be {age + 1} years old")
