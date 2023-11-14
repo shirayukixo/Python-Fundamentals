71e1a2c5755b4a54f79bc5a7ebf35c168fe0db3e
Suet Yee <suetyeeee@gmail.com>
2023-11-14 13:25:25 +0800

    Update

---
+++ finky_code.py
@@ -0,0 +1,35 @@
+char = "#"
+
+print("")
+
+print(f" {char*4}")
+print(f"{char} ")
+print(f"{char} ")
+print(f"{char} ")
+print(f" {char*4}")
+
+print("")
+
+print(f" {char*4}")
+print(f"{char}   {char}")
+print(f"{char}   {char}")
+print(f"{char}   {char}")
+print(f" {char*4}")
+
+print("")
+
+print(f"{char*3}")
+print(f"{char}  {char}")
+print(f"{char}   {char}")
+print(f"{char}  {char}")
+print(f"{char*3}")
+
+print("")
+
+print(f"{char*5}")
+print(f"{char}")
+print(f" {char*4}")
+print(f"{char}")
+print(f"{char*5}")
+
+print("")
