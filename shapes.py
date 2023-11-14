4e57d5fb026c2c8becf13f43ec20b0a60a53c25f
Suet Yee <suetyeeee@gmail.com>
2023-11-14 13:34:06 +0800

    Update

---
+++ shapes.py
@@ -0,0 +1,12 @@
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+print(f"The area of the circle with radius {radius} is: {radius**2 * math.pi}")
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+print(f"The area of the triangle with base {base} and height {height} is: {1/2 * base * height}")
+
+length = float(input("Enter the length of the rectange: "))
+breadth = float(input("Enter the breadth of the rectange: "))
+print(f"The area of the rectangle with length {length} and breadth {breadth} is: {length*breadth}")
